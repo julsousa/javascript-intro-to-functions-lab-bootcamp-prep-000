8a878bbc9ace365fb426a81e0c0de37acea69f59
JulieSousa Sousa <juliesousa@yahoo.com>
2018-02-26 20:29:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function shout(string) {
 function whisper(string) {
     return string.toLowerCase()
 }
-function logShout(string) {
-      console.log()
+function logShout(spy) {
+      console.log('${spy}')
 
 }
 
